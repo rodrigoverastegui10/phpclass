090a5337276d8c78dd46f345e67344122137a53e
Rodrigo <rodrigo.verastegui@icloud.com>
2025-09-22 19:22:08 -0500

    finish 8 ball

--- tests/get-post.php
+++ tests/get-post.php
@@ -8,17 +8,16 @@
 //
     $firstName = $_POST['firstName'] ?? '';
     $lastName = $_POST['lastName'] ?? '';
-?>
-<!DOCTYPE html>
+?><!DOCTYPE html>
 <html>
 <head>
     <title>Testing Get and Post</title>
 </head>
 <body>
-    <h2><Welcome <?=$firstName?><?=$lastName?>/h2>
+    <h2><Welcome <?=$firstName;?><?=$lastName;?>/h2>
     <form method="post">
-        <input type="text" name="firstName" id="firstName" value="<?=$firstName?>"/>
-        <input type="text" name="lastName" id="lastName" value="<?=$lastName?>"/>
+        <input type="text" name="firstName" id="firstName" value="<?=$firstName;?>"/>
+        <input type="text" name="lastName" id="lastName" value="<?=$lastName;?>"/>
         <input type="submit" value="Submit Data">
     </form>
 
